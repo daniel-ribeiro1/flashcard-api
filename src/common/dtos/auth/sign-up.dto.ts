f3e6055e448c782c2ea3c2d20087577e6121f4dc
Daniel R. de OLiveira <daniel.ribeiro.dev@outlook.com>
2025-01-12 13:17:57 -0300

    refactor(validation): validar os dtos dos endpoint

--- src/common/dtos/auth/sign-up.dto.ts
+++ src/common/dtos/auth/sign-up.dto.ts
@@ -3,7 +3,6 @@ import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 // TODO:
 //  - Adicionar mensagens internacionalizadas para as valiadções
-//  - Fazer com que a validação seja aplicada
 export class SignUpDto
   implements
     Pick<User, 'first_name' | 'last_name' | 'email' | 'password' | 'picture'>
--- src/modules/common.module.ts
+++ src/modules/common.module.ts
@@ -1,9 +1,20 @@
 import { PrismaService } from '@/services/prisma.service';
-import { Global, Module } from '@nestjs/common';
+import { Global, Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
 
 @Global()
 @Module({
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_PIPE,
+      useFactory: () => {
+        return new ValidationPipe({
+          whitelist: true,
+        });
+      },
+    },
+  ],
   exports: [PrismaService],
 })
 export class CommonModule {}
